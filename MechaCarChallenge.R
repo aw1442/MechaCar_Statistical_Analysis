2a60a518c7ca651ad24067625b23ebea49a465f1
Alex Wang <wang.alex92@gmail.com>
2021-05-26 17:10:21 -0700

    message

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+# Deliverable 1
+
 library(dplyr)
 
 library(tidyverse)
@@ -6,3 +8,18 @@ mecha_mpg <- read.csv(file='./Resources/MechaCar_mpg.csv',check.names=F,stringsA
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)) 
+
+# Deliverable 2
+
+mecha_coil <- read.csv(file='./Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
+
+total_summary <- mecha_coil %>% summarize(Mean=mean(PSI),
+                                          Median=median(PSI),
+                                          Variance=var(PSI),
+                                          SD=sd(PSI), .groups = 'keep') 
+
+lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                         Median=median(PSI),
+                                                                         Variance=var(PSI),
+                                                                         SD=sd(PSI), .groups = 'keep')  
+
