65c8cc7d7816f0e4dd1e2f4773e90a9c92505c88
aw1442 <76754655+aw1442@users.noreply.github.com>
2021-05-27 10:47:18 -0700

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,14 @@ lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=me
                                                                          Variance=var(PSI),
                                                                          SD=sd(PSI), .groups = 'keep')  
 
+# Deliverable 3
+
+t.test(mecha_coil$PSI,mu=1500)
+
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
